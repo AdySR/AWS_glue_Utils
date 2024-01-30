697c01bfc9d46ce6b3623811faa98b4288f55d75
Aditya Rathi <31534457+AdySR@users.noreply.github.com>
2024-01-31 01:15:16 +0530

    Update list_files_ordered.py

--- list_files_ordered.py
+++ list_files_ordered.py
@@ -1,7 +1,14 @@
 import boto3
+import re
 
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket('s3-cal')
 files = my_bucket.objects.filter()
 files = [obj.key for obj in sorted(files, key=lambda x: x.last_modified, 
     reverse=True)]
+
+
+
+r = re.compile('^2\d{3}\/')
+filtered_files = list(filter(r.match, files)) # Read Note below
+print(filtered_files)
